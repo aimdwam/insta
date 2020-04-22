df0279ab771cdbc620f64cbac9d7051f11d97987
Chris Cheung <aimdwam@gmail.com>
2020-04-22 20:29:36 +0800

    introduce async/await syntax

--- App.js
+++ App.js
@@ -6,31 +6,24 @@ import { Camera } from 'expo-camera';
 // 2 pages, 1. taking picture, 2. display them
 
 export default function App() {
-
-  // [theState, theSetStateFunction] = useState(initialState)
   const [granted, setGranted] = useState(false)
 
-  // please execute the function I provide when first mount/render this component (App)
-  // also whenever the dependencies changed (2nd param array)
   useEffect(() => {
-    // use effect
-    const x = // a promise of number
-    Camera.requestPermissionsAsync().then(response => {
+    // const fn = async () => {
+    //   console.log("first")
+    //   const resolvedValue = await promise
+    //   console.log("second")
+    // }
+
+    (async () => {
+      const response = await Camera.requestPermissionsAsync()
       setGranted(response.granted)
-    }, reason => {
-      console.log(reason)
-    })
-    console.log('use effect')
+    }) ()
   }, [])
 
   return (
     <View style={styles.container}>
       <Text style={{fontSize: 40}}>hello world</Text>
-      {
-        // JS expression
-        // must be JSX (elem/node)
-        // if this expression is falsy (false, "", []), will be skipped
-      }
       {
         granted &&
         <Camera style={{width: 100, height: 100}} />
