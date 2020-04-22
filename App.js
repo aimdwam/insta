24095055e9b84f680d5713afac907ed2e9d3e094
Chris Cheung <aimdwam@gmail.com>
2020-04-22 20:10:53 +0800

    added a fixed size camera

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Camera } from 'expo-camera';
+
+// Insta app
+// 2 pages, 1. taking picture, 2. display them
 
 export default function App() {
+
+  // [theState, theSetStateFunction] = useState(initialState)
+  const [granted, setGranted] = useState(false)
+
+  // please execute the function I provide when first mount/render this component (App)
+  // also whenever the dependencies changed (2nd param array)
+  useEffect(() => {
+    // use effect
+    const x = // a promise of number
+    Camera.requestPermissionsAsync().then(response => {
+      setGranted(response.granted)
+    }, reason => {
+      console.log(reason)
+    })
+    console.log('use effect')
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontSize: 40}}>hello world</Text>
+      {
+        // JS expression
+        // must be JSX (elem/node)
+        // if this expression is falsy (false, "", []), will be skipped
+      }
+      {
+        granted &&
+        <Camera style={{width: 100, height: 100}} />
+      }
     </View>
   );
 }
@@ -12,7 +42,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#aaa',
     alignItems: 'center',
     justifyContent: 'center',
   },
