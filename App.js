5343faf9a40bf6e05fc10a763f1b1296a3f1a79b
Chris Cheung <aimdwam@gmail.com>
2020-04-23 09:47:43 +0800

    introduce react-native-paper to the project

--- App.js
+++ App.js
@@ -2,19 +2,22 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Camera } from 'expo-camera';
 import CamView from './components/CamView';
-// import { Provider as PaperProvider } from 'react-native-paper';
+import { Provider, Button } from 'react-native-paper';
 
 // Insta app
 // 2 pages, 1. taking picture, 2. display them
 
 export default function App() {
   return (
-    // <PaperProvider>
+    <Provider>
     <View style={styles.container}>
       <Text style={{fontSize: 40}}>hello world</Text>
       <CamView/>
+      <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+        Press me
+      </Button>
     </View>
-    // </PaperProvider>
+    </Provider>
   );
 }
 
